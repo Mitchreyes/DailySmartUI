1026890ff4cd69a5300e1eb25a0d3abf753e2b89
Mitchreyes <mitchreyes@gmail.com>
2020-01-13 07:39:10 -0800

    Created searchbar, recent posts, and logo components and integrated into app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import Logo from './logo';
+import SearchBar from './searchbar';
+import RecentPosts from './recentposts';
 
 export default class App extends Component {
 	render() {
 		return (
-			<div className="app">
-				<h1>DevCamp React Starter</h1>
-				<h2>React Redux Router</h2>
+			<div class="container">
+				<div class="logo">
+					<Logo />
+				</div>
+
+				<div class="search">
+					<SearchBar />
+				</div>
+
+				<div class="recent">
+					<RecentPosts />
+				</div>
 			</div>
 		);
 	}
