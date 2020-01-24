8959c8111f6586d2840675825b2ccab20a254edc
Mitchreyes <mitchreyes@gmail.com>
2020-01-24 14:59:38 -0800

    styled the results component searchbar

--- src/components/results.js
+++ src/components/results.js
@@ -13,15 +13,9 @@ class Results extends Component {
 	render() {
 		return (
 			<div className="results-wrapper">
-				<div className="logo-container">
-					<Logo size={150} />
-				</div>
-				<div className="search-bar-container">
-					<SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
-				</div>
-				<div className="results-posts-container">
-					<ResultsPosts />
-				</div>
+				<Logo size={150} />
+				<SearchBar page="results" onSubmit={(query) => this.handleSearchBarSubmit(query)} />
+				<ResultsPosts />
 			</div>
 		);
 	}
